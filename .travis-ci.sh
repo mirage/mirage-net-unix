7f8338853a4fa68e560c227260a8c7a730ccb253
Anil Madhavapeddy <anil@recoil.org>
2014-01-19 23:39:32 +0000

    travis: correct io-page dep

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,5 @@
 # OPAM packages needed to build tests.
-OPAM_PACKAGES="cstruct ounit lwt mirage-types io-page-unix tuntap"
+OPAM_PACKAGES="cstruct ounit lwt mirage-types io-page tuntap"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
 3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
