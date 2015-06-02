6b2eee48dddd287b2db62306ed62e324bd00f452
Thomas Gazagnaire <thomas@gazagnaire.org>
2015-06-02 09:53:32 +0200

    Update licenses

--- lib/netif.mli
+++ lib/netif.mli
@@ -1,5 +1,6 @@
 (*
- * Copyright (C) 2013 Anil Madhavapeddy <anil@recoil.org>
+ * Copyright (C) 2013-2015 Anil Madhavapeddy <anil@recoil.org>
+ * Copyright (C) 2015      Thomas Gazagnaire <thomas@gazagnaire.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
